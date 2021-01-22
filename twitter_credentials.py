e783fa12e12f857a83daa8243472660929a0254e
tshark101A <45115876+tshark101A@users.noreply.github.com>
2021-01-22 22:48:04 +0530

    Twitter Sentiment Analysis

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -4,7 +4,9 @@ Created on Tue Nov  3 11:47:34 2020
 
 @author: Lenovo
 """
-Access_key="1323330610262208512-DyiUpLNEkmTOWGixqaiuwXgpr7UWTX"
-Access_key_secret="IgJiQOl8IZdMc8Da87bPwjBsJs9m9MLgA3WA6fohqZOzP"
-Consumer_key="8xzjbSJTnpY0vgUTM2WGgPiCX"
-Consumer_key_secret="Uio8PISyOKjtNDjE3crhrzPIiOI7fYL25JxUJyXIRCJjDkfhB5"
+
+#Create a twitter developers account and enter the credentials.
+Access_key="Enter access key"
+Access_key_secret="Enter access key secret"
+Consumer_key="Enter Consumer Key"
+Consumer_key_secret="Enter Consumer Key Secret"
